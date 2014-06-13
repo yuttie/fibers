609d2878455ada3103aff3fafa03025598b693ae
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-06-13 16:10:44 +0900

    Manage stack of source IDs

--- Spinner.hs
+++ Spinner.hs
@@ -11,15 +11,29 @@ import           System.IO
 type Key = Text
 type Value = Text
 data Fiber = Exist !Key !Value
+type SourceID = Text
 
-data Source = HaveOutput Source Fiber
+data Source = Init Source SourceID
+            | HaveOutput Source Fiber
             | NeedIO (IO Source)
+            | Finishing Source SourceID
             | Done
+
+session :: SourceID -> Source -> Source
+session sid src = Init src sid `combine` Finishing Done sid
+
+ini :: Source -> SourceID -> Source
+ini = Init
+
+fin :: SourceID -> Source
+fin = Finishing Done
 
 data Sink = NeedInput (Fiber -> IO Sink)
           | Closed
 
 combine :: Source -> Source -> Source
+combine (Init s sid) s' = Init (s `combine` s') sid
+combine (Finishing s sid) s' = Finishing (s `combine` s') sid
 combine Done s' = s'
 combine (NeedIO a) s' = NeedIO $ (`combine` s') <$> a
 combine (HaveOutput s fib) s' = HaveOutput (s `combine` s') fib
@@ -27,7 +41,9 @@ combine (HaveOutput s fib) s' = HaveOutput (s `combine` s') fib
 sourceFile :: FilePath -> Source
 sourceFile fp = NeedIO $ do
     ls <- T.lines <$> T.readFile fp
-    return $ foldr (\l src -> HaveOutput src $ Exist l "1") Done ls
+    let sid = T.pack fp
+    let src = foldr (\l next -> HaveOutput next $ Exist l "1") (fin sid) ls
+    return $ ini src sid
 
 sinkHandle :: Handle -> Sink
 sinkHandle h = self
@@ -38,20 +54,32 @@ sinkHandle h = self
             return self
 
 spin :: Source -> Sink -> IO ()
-spin = go
+spin = go []
   where
-    go Done _ = return ()
-    go (NeedIO a) sink = do
+    go stk (Init s sid) sink = do
+        hPutStr stderr "begin \"" >> T.hPutStr stderr sid >> hPutStrLn stderr "\""
+        go (sid : stk) s sink
+    go [] (Finishing _ _) _ = fail "nothing to pop"
+    go (sid:stk) (Finishing s sid') sink
+      | sid /= sid' = fail "failed to pop"
+      | otherwise = do
+          hPutStr stderr "end \"" >> T.hPutStr stderr sid >> hPutStrLn stderr "\""
+          go stk s sink
+    go stk Done _ = return ()
+    go stk (NeedIO a) sink = do
         src <- a
-        go src sink
-    go (HaveOutput src fib) sink = case sink of
+        go stk src sink
+    go stk (HaveOutput src fib) sink = case sink of
         Closed -> return ()
         NeedInput put -> do
             sink' <- put fib
-            go src sink'
+            go stk src sink'
 
 main :: IO ()
 main = do
-    let src = sourceFile "test_in.dat"
+    let src1 = sourceFile "test1.dat"
+    let src2 = sourceFile "test2.dat"
+    let src3 = sourceFile "test3.dat"
+    let src = src1 `combine` src2 `combine` src3
     let sink = sinkHandle stdout
     spin src sink
