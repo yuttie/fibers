36a7fb201aaa8686660a74633e52f8f28c523215
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-06-15 12:21:57 +0900

    sourceFile now uses MonadResource m to manage file handle

--- Test.hs
+++ Test.hs
@@ -1,21 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Applicative    ((<$>))
-import           Control.Monad.IO.Class (MonadIO (..), liftIO)
-import qualified Data.Text              as T
-import qualified Data.Text.IO           as T
+import           Control.Applicative          ((<$>))
+import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.Trans.Resource (MonadResource (..), allocate,
+                                               release, runResourceT)
+import qualified Data.Text                    as T
+import qualified Data.Text.IO                 as T
 import           System.IO
 
 import           Spinner
 
 
-sourceFile :: MonadIO m => FilePath -> Source m
+sourceFile :: MonadResource m => FilePath -> Source m
 sourceFile fp = NeedIO $ do
-    ls <- liftIO $ T.lines <$> T.readFile fp
-    let sid = T.pack fp
-    let src = foldr (\l next -> HaveOutput next $ Exist l "1") (fin sid) ls
-    return $ ini src Done sid
+    (relKey, h) <- allocate (openFile fp ReadMode) hClose
+    return $ ini (go relKey h) Done sid
+  where
+    sid = T.pack fp
+    go relKey h = NeedIO $ do
+        isEOF <- liftIO $ hIsEOF h
+        if isEOF
+            then do
+                release relKey
+                return $ fin sid
+            else do
+                l <- liftIO $ T.hGetLine h
+                return $ HaveOutput (go relKey h) (Exist l "1")
 
 main :: IO ()
 main = do
@@ -24,4 +35,4 @@ main = do
     let src3 = sourceFile "test3.dat"
     let src = dedup $ src1 `combine` src1 `combine` src2 `combine` src3 `combine` src2
     let sink = sinkHandle stdout
-    spin src sink
+    runResourceT $ spin src sink
