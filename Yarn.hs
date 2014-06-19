7937c38aa15ee270d14cf639ad634d88bfdf828f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-06-19 11:46:41 +0900

    Add foldl'

--- Yarn.hs
+++ Yarn.hs
@@ -61,6 +61,40 @@ foldl f z y = do
                 return x'
     !h = unYarn y
 
+foldl' :: (a -> Fiber -> a) -> a -> Yarn -> IO a
+foldl' f z y = do
+    IO.hSeek h IO.AbsoluteSeek 0
+    loop id z
+  where
+    loop front x = do  -- more bytes are needed
+        eof <- IO.hIsEOF h
+        if eof
+            then finish front x
+            else do
+                buf <- LBS.hGet h 4096
+                go front buf x
+    go front more x = do  -- 'more' is always the beginning of a line
+        let (first, second) = LBS.break (== '\n') more
+        case LBS.uncons second of
+            Nothing -> loop (LBS.append $ front more) x
+            Just (_, second') -> do
+                let l = front first
+                if LBS.null l
+                    then fail "Yarn.foldl: an empty line"
+                    else do
+                        let fib = either (error . ("Yarn.foldl: " ++)) id $ Aeson.eitherDecode l
+                        let !x' = f x fib
+                        go id second' x'
+    finish front x = do
+        let l = front LBS.empty
+        if LBS.null l
+            then return x
+            else do
+                let fib = either (error . ("Yarn.foldl: " ++)) id $ Aeson.eitherDecode l
+                let !x' = f x fib
+                return x'
+    !h = unYarn y
+
 insert :: Fiber -> Yarn -> IO ()
 insert fib y = do
     IO.hSeek h IO.SeekFromEnd 0
